789f4c7dc2c8d5073daadbefc5c313fb3198bd6d
adgal <gallet.adrian@gmail.com>
2018-05-13 20:18:58 +0200

    Art House fonctionnel : montage à la racine du this.state de App.js pour le moment (besoin de correction setParameter pour montage sur this.state.parameters.artHouse)

--- src/filterbyarthouse/filterbyarthouse.js
+++ src/filterbyarthouse/filterbyarthouse.js
@@ -6,14 +6,28 @@ class Searchbar extends Component {
 	constructor(props) {
 		super(props);
 
+		this.state = {
+			artHouse: 2
+		}
+
 		this.updateArtHouse = this.updateArtHouse.bind(this);
+		this.handleChange = this.handleChange.bind(this);
 		this.search = this.search.bind(this);
 	}
 
 	// UPDATE ART HOUSE IN APP
 	updateArtHouse(e) {
-		this.props.setParentArtHouse(e);
+		this.props.setParentArtHouse(e.target.value);
 	}
+
+	// UPDATE ART HOUSE STATE IN Component
+	handleChange(event) {
+	  this.setState({
+	    artHouse: event.target.value
+	  });
+		this.updateArtHouse(event);
+	}
+
 
 	search(){
 		this.props.search();
@@ -24,18 +38,33 @@ class Searchbar extends Component {
 		return (
 			<div className="filter artHouse">
 				<h2>Art house</h2>
-				<p className="radio">
-					<input type="radio" id="both" name="artHouse" value={2} onClick={ value => this.updateArtHouse({value})} /><label htmlFor="both">Except</label>
-				</p>
-				<p className="radio">
-					<input type="radio" id="except" name="artHouse" value={1} onClick={ value => this.updateArtHouse({value})} /><label htmlFor="except">Except</label>
-				</p>
-				<p className="radio">
-					<input type="radio" id="only" name="artHouse" value={0} onClick={ value => this.updateArtHouse({value})} /><label htmlFor="only">Only</label>
-				</p>
+				<ul>
+				<li className="radio">
+					<input type="radio" id="both" name="artHouse" value={"2"}
+					checked={this.state.artHouse === "2"}
+					onChange={ this.handleChange } /><label htmlFor="both">Both</label>
+					<div className="check"></div>
+				</li>
+				<li className="radio">
+					<input type="radio" id="except" name="artHouse" value={"1"} checked={this.state.artHouse === "1"}
+					onChange={ this.handleChange } /><label htmlFor="except">Except</label>
+					<div className="check">
+						<div className="inside"></div>
+					</div>
+				</li>
+				<li className="radio">
+					<input type="radio" id="only" name="artHouse" value={"0"} checked={this.state.artHouse === "0"}
+					onChange={ this.handleChange } /><label htmlFor="only">Only</label>
+					<div className="check">
+						<div className="inside"></div>
+					</div>
+				</li>
+				</ul>
 			</div>
 		);
 	}
 }
 
+
+/* onClick={ value => this.updateArtHouse({value})} */
 export default Searchbar;
