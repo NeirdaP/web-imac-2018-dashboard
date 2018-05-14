9ca650b5e4ae96a051078cddc011545ccfca08da
michelYip <wu.michel.yip@gmail.com>
2018-05-14 11:47:54 +0200

    php artisan wn:model

---
+++ back/app/Model/Frequenting.php
@@ -0,0 +1,17 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Frequenting extends Model {
+
+    protected $fillable = [];
+
+    protected $dates = [];
+
+    public static $rules = [
+        // Validation rules
+    ];
+
+    // Relationships
+
+}
